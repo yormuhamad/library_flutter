9f8e72891dcc2127dc3fdb8ef64d46cf5867ce21
yormuhammad <yormuhammadodinaev1991@gmail.com>
2024-07-25 18:00:16 +0500

    first commit

---
+++ books/lib/main.dart
@@ -0,0 +1,38 @@
+
+
+
+import 'package:books/common/color_extenstion.dart';
+import 'package:books/view/main_tab/main_tab_view.dart';
+import 'package:books/view/onboarding/onboarding_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primaryColor: TColor.primary,
+      
+        fontFamily: 'SF Pro Text',
+      ),
+      home: const OnboardingView(),
+    );
+  }
+}
